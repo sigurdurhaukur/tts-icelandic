1835618bc4ce9bd3bc5f787ead3e7f26a6c0dc93
sigurdurhaukur <56935611+sigurdurhaukur@users.noreply.github.com>
2025-02-16 13:26:58 +0100

    asdfasdf

--- inference.sh
+++ inference.sh
@@ -10,3 +10,9 @@ python3 fish-speech/fish_speech/models/text2semantic/inference.py \
 python3 fish-speech/fish_speech/models/vqgan/inference.py \
     -i "temp/codes_0.npy" \
     --checkpoint-path "checkpoints/fish-speech-1.5/firefly-gan-vq-fsq-8x1024-21hz-generator.pth"
+
+python3 -m tools.api_client \
+    --text "Einu sinni var drengur að nafni Haukur sem elskaði að klifra í trjám og rannsaka dularfulla staði í skóginum nálægt heimili sínu. Dag einn, þegar hann var að klifra í gömlum eikartré, sá hann stóran svartan hrafn sem sat á grein og horfði á hann með glitrandi augum." \
+    --reference_audio "haukur.wav" \
+    --reference_text "Halló, halló. Þetta er einnhver að tala. Bara tala, tala, og segja mörg orð á svoldið glaðan máta." \
+    --streaming True
